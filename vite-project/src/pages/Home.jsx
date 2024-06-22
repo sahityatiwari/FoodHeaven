2b40d5484d14395c576b2bb8ffaf82b79da583de
Sahitya Tiwari <sahityatiwari90035@gmail.com>
2024-06-22 18:11:46 +0530

    Update Home.jsx

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Mainpage from '../components/mainpage';
 import About from '../components/about';
 import Qualities from '../components/Qualities';
 import Team from '../components/Team';
-import Footer from '../components/Footer';
+
 import Menu from '../components/Menu';
 import WhoAreWe from '../components/WhoAreWe';
 import Reservation from '../components/Reservation';
