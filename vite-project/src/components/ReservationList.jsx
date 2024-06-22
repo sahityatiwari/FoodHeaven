11d12f8f7ae4298395a93bb00a423442c9c8797d
Sahitya Tiwari <sahityatiwari90035@gmail.com>
2024-06-22 13:56:59 +0530

    Update ReservationList.jsx

--- vite-project/src/components/ReservationList.jsx
+++ vite-project/src/components/ReservationList.jsx
@@ -11,7 +11,7 @@ const ReservationList = () => {
   useEffect(() => {
     const fetchReservations = async () => {
       try {
-        const response = await axios.get('http://localhost:2025/api/v1/reservation/askdata');
+        const response = await axios.get('https://food-heaven-fawn.vercel.app/api/v1/reservation/askdata');
         setReservations(response.data);
       } catch (error) {
         console.error('Error fetching reservations:', error);
